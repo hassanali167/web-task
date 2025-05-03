d696efbec4666db6fa63ed68780259bc02e8048b
AarkayITsolutions <alihassanali119683@gmail.com>
2025-05-03 16:25:37 +0500

    Adding files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import CreateUser from './components/CreateUser';
+import UpdateUser from './components/UpdateUser';
+
+function App() {
+  return (
+    <div className="App">
+      <CreateUser />
+      <hr />
+      <UpdateUser />
+    </div>
+  );
+}
+
+export default App;
