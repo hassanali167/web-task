e994ca689b9f838145adde712e906ca4eece5be6
Hassan Ali <alihassanali119683@gmail.com>
2025-05-12 14:36:52 +0500

    Update CreateUser.jsx

--- frontend/src/components/CreateUser.jsx
+++ frontend/src/components/CreateUser.jsx
@@ -3,7 +3,6 @@ import axios from 'axios';
 
 export default function CreateUser() {
   const [user, setUser] = useState({ name: '', email: '' });
-
   
   const handleSubmit = async (e) => {
     e.preventDefault();
